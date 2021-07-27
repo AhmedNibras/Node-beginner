08bd0eb4ef46f892d30d89320c6a1e9ab3a1ffbd
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-07-27 22:54:09 +0500

    Adding fs-sync.js

--- app.js
+++
@@ -1,6 +0,0 @@
-const { readFileSync, writeFileSync } = require('fs');
-const first = readFileSync('./content/first.txt', 'utf8')
-const second = readFileSync('./content/second.txt', 'utf8')
-
-console.log(first, second)
-

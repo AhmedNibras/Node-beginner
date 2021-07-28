afe4a27615f60bffcd561ac05557356ab07803b4
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-07-28 14:58:29 +0500

    Adding sever and blocking

---
+++ block.js
@@ -0,0 +1,22 @@
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+    if(req.url === '/') {
+        res.end('This is Homepage')
+    }
+    if(req.url === '/about')
+    {
+        // Blocking Code!!!
+        for(let i = 0; i <1000; i++){
+            for(let j = 0; j <1000; j++) {
+                console.log(`${i} ${j}`)
+            }
+        }
+        res.end('About Page')
+    }
+    res.end('Error Page')
+})
+
+server.listen(5000, () => {
+    console.log('Server listening on port : 5000....')
+})

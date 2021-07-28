4f01842b1e5cf925c303f9179992188b5ab25cf3
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-07-28 16:29:48 +0500

    read and write file

---
+++ app.js
@@ -0,0 +1,36 @@
+const {readFile, writeFile} = require('fs')
+const util = require('util')
+const readFilePromise = util.promisify(readFile)
+const writeFilePromise = util.promisify(writeFile)
+
+
+const start = async() => {
+    try{
+        const first = await readFilePromise('./content/first.txt', 'utf8')
+        const second = await readFilePromise('./content/second.txt', 'utf8')
+        await writeFilePromise('./content/result-mind-gernade.txt', `This Is Great: ${first} ${second}`)
+        console.log(first, second)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+start()
+
+// const getText = (path) =>{
+//     return new Promise((resolve, reject) =>{
+        
+//         readFile(path, 'utf8', (err, data) => {
+//             if(err){
+//                 reject(err);
+//             }
+//             else{
+//                 resolve(data)
+//             }
+//         })
+//     })
+// }
+
+// getText('./content/first.txt')
+//     .then((result) => console.log(result))
+//     .catch((err) => console.log(err))

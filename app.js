91d0ebd9c1828f315b0a60ae20c8463c3bb5bd78
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-07-31 01:15:42 +0500

    Adding navbar app

--- app.js
+++ app.js
@@ -2,20 +2,43 @@ const http = require('http')
 const {readFileSync} = require('fs')
 
 //get all files
-const homePage = readFileSync('./index.html')
+const homePage = readFileSync('./navbar-app/index.html')
+const homeStyle = readFileSync('./navbar-app/styles.css')
+const homeImage = readFileSync('./navbar-app/logo.svg')
+const homeLogic = readFileSync('./navbar-app/browser-app.js')
 
 const server = http.createServer((req, res) => {
 
+    //console.log(req.method)
     const url = req.url;
 
+    //HomePage
     if(url === '/') {
-        res.writeHead(200, {'content-type': 'text/html'});
+        res.writeHead(200, { 'content-type': 'text/html'});
         res.end(homePage)
         res.end()
     }
+    //about page
     else if(url === '/about') {
         res.writeHead(200, {'content-type': 'text/html'})
         res.write('<h1>about page</h1>')
+        res.end()
+    }
+    // Style
+    else if(url === '/styles.css') {
+        res.writeHead( 200, {'content-type': 'text/css' })
+        res.write(homeStyle)
+        res.end()
+    }
+
+    else if(url === '/logo.svg') {
+        res.writeHead(200, {'content-type': 'image/svg+xml'})
+        res.write(homeImage)
+        res.end()
+    }
+    else if(url === '/browser-app.js') {
+        res.writeHead(200, {'content-type': 'text/javascript'})
+        res.write(homeLogic)
         res.end()
     }
     else{

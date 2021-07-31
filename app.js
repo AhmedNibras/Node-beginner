73308290ad7ba486858aa8bfc90fb7f63e4215e4
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-07-31 16:37:34 +0500

    Adding single product item

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const express = require('express')
 const app = express()
+const { products } = require('./name')
 
 app.get('/', (req, res) => {
-    res.json([
-    {
-        name: 'Nibras',
-    },
-    { 
-        name: 'Ahmed'
-    }])
+    res.send('<h1> Home </h1> <a href="/api/products">products</a>') 
 })
 
-app.listen(5000, (req, res) => {
+app.get('/api/products', (req, res) => {
+    const singleProduct = products.find((product) => product.id === 1)
+    
+    res.json(newProducts)
+}
+
+app.listen(5000, () => {
     console.log('Server is listening on port 5000....')
 });

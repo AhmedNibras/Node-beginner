53f8c486e3f40c87d60f4e9baa1c06bbbc2b9124
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-07-31 17:03:51 +0500

    Showing single product in url

--- app.js
+++ app.js
@@ -1,21 +1,35 @@
 const express = require('express')
 const app = express()
-const { products } = require('./name')
+const { products } = require('./datas')
 
 app.get('/', (req, res) => {
-    res.send('<h1> Home </h1> <a href="/api/products">products</a>') 
+    res.send('<h1> Home Page</h1> <a href = "/api/products">products</a>')
+})
+app.get('/api/products', (req, res) => { 
+
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product
+        return {id, name, image}
+    })
+    res.json(newProducts)
 })
 
-app.get('/api/products/:productID', (req, res) => {
+app.get('/api/products/:productsID', (req, res) => {
     // console.log(req)
     // console.log(req.params)
 
     const {productsID} = req.params;
 
-    const singleProduct = products.find(
+     const singleProduct = products.find(
         (product) => product.id === Number(productsID)
     )
-    res.json(singleProduct )
+
+    if(!singleProduct)
+    { 
+        return res.status(404).send('Product doesnot exist')
+    }
+    
+    return res.json(singleProduct )
 })
 
 app.listen(5000, () => {

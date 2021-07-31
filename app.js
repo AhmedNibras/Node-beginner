7fcbc7caa853cfab6743c93b516c6e1688ea5774
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-07-31 18:12:35 +0500

    query selector

--- app.js
+++ app.js
@@ -37,6 +37,31 @@ app.get('/api/products/:productsID/reviews/:reviewID', (req, res) =>{
     res.send('Hello World')
 })
 
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+    const { search, limit} = req.query
+    let sortedProducts = [...products]
+
+    if(search){
+        sortedProducts = sortedProducts.filter(
+            (product) =>{
+                return product.name.startsWith(search)
+            })
+    }
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if(sortedProducts.length < 1){
+        // res.status(200).send('no products matched your search')
+        return res.status(200).json({
+            sucess: true, 
+            data:[]
+        })
+    }
+    res.status(200).json(sortedProducts)
+    // res.send('Hello World')
+})
+
 app.listen(5000, () => {
     console.log('Server is listening on port 5000....')
 });

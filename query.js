991660729ddfa1c11ffb8433f582d5a5748d2daf
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-08-01 18:15:11 +0500

    Adding query file

---
+++ query.js
@@ -0,0 +1,67 @@
+const express = require('express')
+const app = express()
+const { products } = require('./datas')
+
+app.get('/', (req, res) => {
+    res.send('<h1> Home Page</h1> <a href = "/api/products">products</a>')
+})
+app.get('/api/products', (req, res) => { 
+
+    const newProducts = products.map((product) => {
+        const { id, name, image } = product
+        return {id, name, image}
+    })
+    res.json(newProducts)
+})
+
+app.get('/api/products/:productsID', (req, res) => {
+    // console.log(req)
+    // console.log(req.params)
+
+    const {productsID} = req.params;
+
+     const singleProduct = products.find(
+        (product) => product.id === Number(productsID)
+    )
+
+    if(!singleProduct)
+    { 
+        return res.status(404).send('Product doesnot exist')
+    }
+    
+    return res.json(singleProduct )
+})
+
+app.get('/api/products/:productsID/reviews/:reviewID', (req, res) =>{
+    console.log(req.params)
+    res.send('Hello World')
+})
+
+app.get('/api/v1/query', (req, res) => {
+    console.log(req.query)
+    const { search, limit} = req.query
+    let sortedProducts = [...products]
+
+    if(search){
+        sortedProducts = sortedProducts.filter(
+            (product) =>{
+                return product.name.startsWith(search)
+            })
+    }
+    if(limit) {
+        sortedProducts = sortedProducts.slice(0, Number(limit))
+    }
+    if(sortedProducts.length < 1){
+        // res.status(200).send('no products matched your search')
+        return res.status(200).json({
+            sucess: true, 
+            data:[]
+        })
+    }
+    res.status(200).json(sortedProducts)
+    // res.send('Hello World')
+})
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000....')
+});

ba5c0188e155c00047c84403669f79621c0324f6
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-08-02 23:59:57 +0500

    Postman

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ let { people } = require('./datas');
 
 // static assets
 app.use(express.static('./methods-public'))
-
+// parse form data
 app.use(express.urlencoded({ extended: false}))
-
+// parse json
 app.use(express.json())
 
 app.get('/api/people', (req, res) => {
@@ -30,6 +30,15 @@ app.post('/login', (req, res) => {
         return res.status(200).send(`Welcome ${name}`)
     }
     res.status(401).send('Please Provide Credentials')
+})
+
+app.post('/api/postman/people', (req, res) => {
+    const { name } = req.body
+    if( !name ) {
+        return res
+            .status(400)
+            .json({ success: false, msg: 'please provide name value'})
+        }
 })
 
 app.listen(5000, () => {

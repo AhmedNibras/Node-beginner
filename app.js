90866e3f5b6530fbb0fd69a5769c914b8e80ed82
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-08-03 00:17:25 +0500

    Adding data through postman

--- app.js
+++ app.js
@@ -39,7 +39,28 @@ app.post('/api/postman/people', (req, res) => {
             .status(400)
             .json({ success: false, msg: 'please provide name value'})
         }
+        res.status(201).send({ success: true, data: [...people, name]})
 })
+
+app.put('/api/people/:id', (req, res) => {
+    const { id } = req.params
+    const { name } = req.body
+    
+    const person = people.find((person) => person.id === Number(id))
+
+    if( !person ) {
+        return res
+            .status(404)
+            .json({ success: false, msg: `no person with id ${id}`})
+        }
+        const newPeople = people.map((person) => {
+            if(person.id === Number(id))
+            { person.name = name
+            }
+            return person
+        })
+        res.status(200).json({success:true, data: newPeople})
+} )
 
 app.listen(5000, () => {
     console.log('Server is listening on port 5000')

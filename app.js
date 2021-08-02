57eec5f20c97eaed59c3764baebbc53b6e25f64a
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-08-02 23:58:02 +0500

    Postman

--- app.js
+++ app.js
@@ -7,17 +7,25 @@ app.use(express.static('./methods-public'))
 
 app.use(express.urlencoded({ extended: false}))
 
+app.use(express.json())
+
 app.get('/api/people', (req, res) => {
-    res.status(404).json({success:true, data:people});
+    res.status(200).json( { success:true, data: people});
 })
 
 app.post('/api/people', (req, res) => {
-    res.status(201).send('success');
+    const { name } = req.body
+    if( !name ) {
+    return res
+        .status(400)
+        .json({ success: false, msg: 'please provide name value'})
+    }
+    res.status(201).send( { success:true, person: name } )
 })
 
 app.post('/login', (req, res) => {
 
-    const {name } = req.body
+    const { name } = req.body
     if(name) {
         return res.status(200).send(`Welcome ${name}`)
     }

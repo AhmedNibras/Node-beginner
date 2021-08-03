81bf33bbbb2cd6cf92a3d4d2b4613accad558ca0
Nibras-Ahmed <nibrasahmad125@gmail.com>
2021-08-03 23:29:02 +0500

    Adding other method of route

--- routes/people.js
+++ routes/people.js
@@ -11,15 +11,14 @@ const {
     deletePerson 
     } = require('../controller/people')
 
-router.get('/', getPeople)
+// router.get('/', getPeople)
+// router.post('/', createPerson)
+// router.post('/postman', createPersonPostman )
+// router.put('/:id', updatePerson)
+// router.delete('/:id', deletePerson)
 
-router.post('/', createPerson)
-
-
-router.post('/postman', createPersonPostman )
-
-router.put('/:id', updatePerson)
-
-router.delete('/:id', deletePerson)
+router.route('/').get(getPeople).post(createPerson);
+router.route('/postman').post(createPersonPostman);
+router.route('/:id').put(updatePerson).delete(deletePerson)
 
 module.exports = router
